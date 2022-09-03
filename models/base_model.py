af77e10e67ad856afb754ecbdb5a829b608fd089
Haywhiz <babarindeademola@gmail.com>
2022-09-03 16:16:56 +0100

    continuation of base models

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,39 @@
+'''base_model.py
+'''
+
+from datetime import datetime
 import uuid
-import datetime
+
 import models
 
+
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
+        if kwargs:
+            for key in kwargs.keys():
+                if key != "__class__":
+                    if key not in ["created_at", "updated_at"]:
+                        self.__setattr__(key, kwargs[key])
+                    else:
+                        self.__setattr__(key, datetime.fromisoformat(kwargs[key]))
+                        
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
-        tformat = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items:
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, tformat)
-                else:
-                    self.__dict__[key] = value
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        to_dict = self.__dict__
-        to_dict['__class__'] = self.__class__.__name__
-        to_dict['created_at'] = self.created_at.isoformat()
-        to_dict['updated_at'] = self.updated_at.isoformat()
+        to_dict = self.__dict__.copy()
+        to_dict["__class__"] = self.__class__.__name__
+        to_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        to_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
         return to_dict
-
-    def __str__(self):
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
